f61ee7c449a9cf0c3e323a691665133008312918
palmerigarrett <palmerigarrett@gmail.com>
2020-08-04 16:30:38 -0400

    updating any changes made

--- rest-manager/rest_manager/menu/admin.py
+++ rest-manager/rest_manager/menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
+ 
 # Register your models here.
+ 
+class ManagerAdmin(admin.ModelAdmin):
+  list_display = ("dishTitle",  "created")
+ 
+admin.site.register(models.MenuItem, ManagerAdmin)
--- rest-manager/rest_manager/menu/models.py
+++ rest-manager/rest_manager/menu/models.py
@@ -5,11 +5,11 @@ class MenuItem(models.Model): # MenuItem Class inherits models.Model
     dishTitle = models.CharField(max_length=250) # a varchar
     price = models.IntegerField(default=0) # an integer field
     description = models.TextField(blank=True) # a text field
-    category = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
     created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
 
     class Meta:
         ordering = ["dishTitle"] #ordering by the created field
 
     def __str__(self):
-       return self.title #name to be shown when called
+       return self.dishTitle #name to be shown when called
